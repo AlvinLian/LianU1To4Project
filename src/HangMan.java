9a97fa382b130d83b827d8b986df7adf36e1838f
Alvin <alvinl70@nycstudents.net>
2022-11-27 21:50:58 -0500

    update

--- src/HangMan.java
+++ src/HangMan.java
@@ -2,12 +2,38 @@ public class HangMan {
     private String[] easyWords = {"cat", "mouse", "school"};
     private String[] mediumWords = {""};
     private String[] hardWords = {""};
+    private String difficulty;
     private String secretWord;
+    private int lives = 3;
 
+    public HangMan() {
+        int randomDifficulty = (int)(Math.random() + 3);
+        if(randomDifficulty == 0) {
+            difficulty = "e";
+        } else if (randomDifficulty == 1) {
+            difficulty = "m";
+        } else {
+            difficulty = "h";
+        }
+    }
     public HangMan(String difficulty) {
+        this.difficulty = difficulty;
     }
 
+    public void chooseWord() {
+        int randomNum = (int)(Math.random() + 6);
+        if(difficulty.equals("e")) {
+            secretWord = easyWords[randomNum];
+        } else if(difficulty.equals("m")) {
+            secretWord = mediumWords[randomNum];
+        } else {
+            secretWord = hardWords[randomNum];
+        }
+    }
 
+    public void game() {
+        while()
+    }
 
 
 }
