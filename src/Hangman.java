d123cd7317d8700c6bd89f3b8e245c37b0460c60
Alvin <alvinl70@nycstudents.net>
2022-12-07 00:14:49 -0500

    update

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,13 +1,25 @@
+/**
+ *  This is a class that represents the Hangman game.
+ */
 public class Hangman {
+    /** This is the list of easy words */
     private String[] easyWords = {"cat", "school", "mouse", "water", "house"};
+    /** This is the list of medium difficulty words */
     private String[] mediumWords = {"suspense", "estimate", "prediction", "diversity", "alternative"};
+    /** This is the list of hard words */
     private String[] hardWords = {"oblivious", "onomatopoeia", "omniscient", "enunciate", "acquaintance"};
+    /** This is the difficulty of the Hangman game */
     private String difficulty = "";
+    /** This is the secret word that the user has to guess */
     private String secretWord = "";
+    /** This is the secret word converted to question marks */
     private String secretWordInQuestionMarks = "";
+    /** This stores all the letters the users entered */
     private String lettersUsed = "Letters used: ";
+    /** This is the number of attempts the user is given */
     private int lives = 5;
 
+    /** Default constructor picks a random difficulty out of easy(e), medium(m), or hard(h) */
     public Hangman() {
         int randomDifficulty = (int)(Math.random() * 3);
         if(randomDifficulty == 0) {
@@ -18,14 +30,21 @@ public class Hangman {
             difficulty = "h";
         }
     }
+    /** One parameter constructor initializes difficulty to the difficulty the user chose except random
+     @param difficulty easy(e), medium(m), or hard(h) game mode
+     */
     public Hangman(String difficulty) {
         this.difficulty = difficulty;
     }
 
+    /** returns the randomized difficulty
+     @returns a string of e for easy, m for medium, or h for hard
+     */
     public String toString() {
-        return "The random difficulty is set to  " + difficulty;
+        return "The random difficulty is set to " + difficulty;
     }
 
+    /** chooses a random word from a five word list depending on the selected difficulty */
     public void chooseWord() {
         int randomNum = (int)(Math.random() * 5);
         if(difficulty.equals("e")) {
@@ -37,16 +56,25 @@ public class Hangman {
         }
     }
 
+    /** converts the secret word to question marks and sets it to the instance variable secretWordInQuestionMarks */
     public void setSecretWordInQuestionMarks() {
         for(int i = 1; i <= secretWord.length(); i++) {
             secretWordInQuestionMarks += "?";
         }
     }
 
+    /** checks if the game is still running or over
+     @returns true if lives is greater than 0 and secretWordInQuestionMarks still contains question marks
+     */
     public boolean isGameRunning() {
         boolean isGameRunning = getLives() > 0 && getSecretWordInQuestionMarks().contains("?");
         return isGameRunning;
     }
+
+    /** handles the user's guesses. If guess is correct, question marks in secretWordInQuestionMarks gets replaced with
+     the letter in the indexes necessary and displays a message that user guessed it right. Otherwise,
+     a message tells the user they are wrong and lives is decremented by 1
+     */
     public void game(String guess) {
         String newStr = "";
         if(secretWord.contains(guess)) {
@@ -68,6 +96,9 @@ public class Hangman {
         }
     }
 
+    /** appends the argument passed into the letter parameter and a comma to the lettersUsed instance variable
+     @param letter the letter that the user entered
+     @returns the String type instance variable called lettersUsed with the new letter added*/
     public String addLetterUsed(String letter) {
         if(lettersUsed.substring(14).contains(letter)) {
             return lettersUsed;
@@ -76,15 +107,21 @@ public class Hangman {
 
     }
 
-
+    /** returns the number of lives remaining
+     @returns an integer from zero to five
+     */
     public int getLives() {
         return lives;
     }
 
+    /** returns the value assigned to the instance variable secretWordInQuestionMarks.
+     @returns a String with question marks, letters, or a mixture of both
+     */
     public String getSecretWordInQuestionMarks() {
         return secretWordInQuestionMarks;
     }
 
+    /** prints the secret word and whether the user won or lost */
     public void determineResults() {
         System.out.println("The word was " + "\"" + secretWord + "\"");
         if(lives > 0) {
--- src/HangmanRunner.java
+++ src/HangmanRunner.java
@@ -1,17 +1,23 @@
+// calling the scanner class to read and receive user input
 import java.util.Scanner;
 public class HangmanRunner {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // asking user for difficulty of their choice
         System.out.print("Easy(e), medium(m), hard(h), or random(r)?: ");
         String difficulty = input.nextLine();
 
         if(difficulty.equals("r")) {
+            // game1 object is created if difficulty is set to r
             Hangman game1 = new Hangman();
+            // prints the randomly selected difficulty
             System.out.println(game1.toString());
             game1.chooseWord();
             game1.setSecretWordInQuestionMarks();
 
+            // repeatedly asks user to enter a letter until game1.isGameRunning is false.
+            // methods are called to check if the letter entered is correct or not
             while(game1.isGameRunning()) {
                 System.out.println(game1.getSecretWordInQuestionMarks());
                 System.out.print("Enter a letter: ");
@@ -20,12 +26,16 @@ public class HangmanRunner {
                 game1.game(guess);
                 System.out.println();
             }
+            //prints whether the user won or lost the game
             game1.determineResults();
         } else {
+            // game2 object is created if difficulty is NOT set to r
             Hangman game2 = new Hangman(difficulty);
             game2.chooseWord();
             game2.setSecretWordInQuestionMarks();
 
+            // repeatedly asks user to enter a letter until game2.isGameRunning is false.
+            // methods are called to check if the letter entered is correct or not
             while(game2.isGameRunning()) {
                 System.out.println(game2.getSecretWordInQuestionMarks());
                 System.out.print("Enter a letter: ");
@@ -34,6 +44,7 @@ public class HangmanRunner {
                 game2.game(guess);
                 System.out.println();
             }
+            //prints whether the user won or lost the game
             game2.determineResults();
         }
 
